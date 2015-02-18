0de5a9a8884334b614e1314fd1e1e716ccc599e1
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-02-18 13:15:03 +0000

    Firm latitude and longitude are set to nil if geocoding fails.

--- app/jobs/geocode_firm_job.rb
+++ app/jobs/geocode_firm_job.rb
@@ -2,7 +2,7 @@ class GeocodeFirmJob < ActiveJob::Base
   def perform(firm)
     results = Geocoder.search(firm.full_street_address)
 
-    if results.present?
+    if results.any?
       Stats.increment('radsignup.geocode.firm.success')
 
       firm.latitude = results.first.latitude
@@ -10,6 +10,10 @@ class GeocodeFirmJob < ActiveJob::Base
       firm.save(callbacks: false)
     else
       Stats.increment('radsignup.geocode.firm.failed')
+
+      firm.latitude = nil
+      firm.longitude = nil
+      firm.save(callbacks: false)
     end
   end
 end
--- spec/jobs/geocode_firm_job_spec.rb
+++ spec/jobs/geocode_firm_job_spec.rb
@@ -12,7 +12,7 @@ RSpec.describe GeocodeFirmJob do
   end
 
   describe '#perform' do
-    context 'when the geocode is successful' do
+    context 'when the firm address can be geocoded' do
       let(:address_line_one) { '120 Holborn' }
       let(:address_line_two) { 'London' }
       let(:address_postcode) { 'EC1N 2TD' }
@@ -29,6 +29,26 @@ RSpec.describe GeocodeFirmJob do
 
       it 'the firm is populated with the longitude' do
         expect(firm.longitude).to eql(-0.1085203)
+      end
+    end
+
+    context 'when firm address can not be geocoded' do
+      let(:address_line_one) { '1000 Fantasy Ave' }
+      let(:address_line_two) { 'Neverland' }
+      let(:address_postcode) { 'ABC 123' }
+
+      before do
+        VCR.use_cassette('geocode-no-results') do
+          job.perform(firm)
+        end
+      end
+
+      it 'the firm latitude is set to nil' do
+        expect(firm.latitude).to be_nil
+      end
+
+      it 'the firm longitude is set to nil' do
+        expect(firm.longitude).to be_nil
       end
     end
   end
