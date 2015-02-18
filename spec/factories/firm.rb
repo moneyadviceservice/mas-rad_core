621208ddf3d9bcd28ec8f32dbfb2de0f8ee8ac85
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-02-18 17:09:49 +0000

    Cast lat and long to floats for firm factory instances.
    They are also rounded to six decimal places.

--- spec/factories/firm.rb
+++ spec/factories/firm.rb
@@ -27,8 +27,8 @@ FactoryGirl.define do
     wills_and_probate_percent 15
     other_percent 10
 
-    latitude { Faker::Address.latitude }
-    longitude { Faker::Address.longitude }
+    latitude { Faker::Address.latitude.to_f.round(6) }
+    longitude { Faker::Address.longitude.to_f.round(6) }
 
     before(:create) { |f| f.class.skip_callback(:save, :after, :geocode_if_needed) }
     after(:create) { |f| f.class.set_callback(:save, :after, :geocode_if_needed) }
