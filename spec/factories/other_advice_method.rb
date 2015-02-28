3c469fe2541281701d513ebf32f03d4e0107fd6d
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-02-28 11:10:43 +0000

    Use faker generated sentence for other advice method `cy_name`.

--- spec/factories/other_advice_method.rb
+++ spec/factories/other_advice_method.rb
@@ -1,6 +1,6 @@
 FactoryGirl.define do
   factory :other_advice_method do
     name { Faker::Lorem.sentence }
-    sequence(:cy_name) { |n| "Dull gyngor arall #{n}" }
+    cy_name { Faker::Lorem.sentence }
   end
 end
