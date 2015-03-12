1166c5addc7da9c596be2449c57096084190faa0
Lawrance Shepstone <lawrance.shepstone@gmail.com>
2015-03-12 13:01:03 +0000

    Adds missing serializer spec for `advises_on_investments`.

--- spec/serializers/firm_serializer_spec.rb
+++ spec/serializers/firm_serializer_spec.rb
@@ -40,6 +40,10 @@ RSpec.describe FirmSerializer do
       expect(subject[:investment_sizes]).to eql(firm.investment_size_ids)
     end
 
+    it 'exposes `advises_on_investments`' do
+      expect(subject[:advises_on_investments]).to be
+    end
+
     it 'exposes `investment_transfers`' do
       expect(subject[:investment_transfers]).to be
     end
