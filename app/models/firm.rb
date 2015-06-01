4f58b034517e9bbdff8c74b064a5a84f893b9f2e
Caden Lovelace <caden@herostrat.us>
2015-06-01 15:37:33 +0100

    Alias `Firm#trading_names` -> `Firm#subsidiaries`

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -26,6 +26,7 @@ class Firm < ActiveRecord::Base
 
   has_many :advisers, dependent: :destroy
   has_many :subsidiaries, class_name: 'Firm', foreign_key: :parent_id, dependent: :destroy
+  has_many :trading_names, class_name: 'Firm', foreign_key: :parent_id, dependent: :destroy
   has_many :qualifications, -> { reorder('').uniq }, through: :advisers
   has_many :accreditations, -> { reorder('').uniq }, through: :advisers
 
