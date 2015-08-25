eaefd2cdbbe9359db3542ce611b2e354754fc383
Alan Gardner <urfolomeus@gmail.com>
2015-08-25 15:27:43 +0100

    Removing removed fields from the field_order

--- app/models/firm.rb
+++ app/models/firm.rb
@@ -149,11 +149,6 @@ class Firm < ActiveRecord::Base
     [
       :email_address,
       :telephone_number,
-      :address_line_one,
-      :address_line_two,
-      :address_town,
-      :address_county,
-      :address_postcode,
       :in_person_advice_methods,
       :other_advice_methods,
       :free_initial_meeting,
