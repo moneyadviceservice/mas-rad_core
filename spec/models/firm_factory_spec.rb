fdd803409168e9548f32711d77edf7e2d78b363f
Dan Munckton <dan@cultivatehq.com>
2015-09-08 14:12:36 +0100

    Tests for firm_with_advisers, firm_with_offices and firm_with_trading_names

--- spec/models/firm_factory_spec.rb
+++ spec/models/firm_factory_spec.rb
@@ -61,6 +61,63 @@ RSpec.describe 'Firm factory' do
     end
   end
 
+  describe 'factory :firm_with_advisers' do
+    let(:factory) { :firm_with_advisers }
+
+    context 'expected status' do
+      it { is_expected.to be_persisted }
+      it { is_expected.to be_valid }
+      it { is_expected.not_to be_publishable }
+      it { is_expected.not_to be_trading_name }
+    end
+
+    context 'associations' do
+      it { expect(subject.principal).not_to be_present }
+      it { is_expected.to have(:no).offices }
+      it { is_expected.to have(3).advisers }
+      it { is_expected.to have(:no).trading_names }
+    end
+  end
+
+  describe 'factory :firm_with_offices' do
+    let(:factory) { :firm_with_offices }
+
+    context 'expected status' do
+      it { is_expected.to be_persisted }
+      it { is_expected.to be_valid }
+      it { is_expected.to be_publishable }
+      it { is_expected.not_to be_trading_name }
+    end
+
+    context 'associations' do
+      it { expect(subject.principal).not_to be_present }
+      it { is_expected.to have(3).offices }
+      it { is_expected.to have(:no).advisers }
+      it { is_expected.to have(:no).trading_names }
+    end
+  end
+
+  describe 'factory :firm_with_trading_names' do
+    let(:factory) { :firm_with_trading_names }
+
+    context 'expected status' do
+      it { is_expected.to be_persisted }
+      it { is_expected.to be_valid }
+      it { is_expected.not_to be_publishable }
+      it { is_expected.not_to be_trading_name }
+    end
+
+    context 'associations' do
+      it { expect(subject.principal).not_to be_present }
+      it { is_expected.to have(:no).offices }
+      it { is_expected.to have(:no).advisers }
+
+      it { is_expected.to have(3).trading_names }
+      it { expect(subject.trading_names).to all(have_attributes(fca_number: subject.fca_number))}
+      it { expect(subject.trading_names).to all(have_attributes(parent: subject))}
+    end
+  end
+
   describe 'factory :firm_with_principal' do
     let(:factory) { :firm_with_principal }
 
