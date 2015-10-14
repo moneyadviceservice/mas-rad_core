6c30074ca60241f0b4ec1fc78339fda72d4d8fee
Mark Connell <mark@markconnell.co.uk>
2015-10-14 13:27:01 +0100

    add system_name method similiar to friendly_name

--- app/models/other_advice_method.rb
+++ app/models/other_advice_method.rb
@@ -1,6 +1,12 @@
 class OtherAdviceMethod < ActiveRecord::Base
   include Translatable
   include FriendlyNamable
+  include SystemNameable
+
+  SYSTEM_NAMES = {
+    1 => :phone,
+    2 => :online
+  }
 
   has_and_belongs_to_many :firms
 
