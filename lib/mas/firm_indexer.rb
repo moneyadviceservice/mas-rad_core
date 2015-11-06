21b77a3602e67e3005632e95109c0d1ab7d72235
Dan Munckton <dan@cultivatehq.com>
2015-11-06 17:45:47 +0000

    Use the singleton class syntax so we can drop the `self.` prefixes

--- lib/mas/firm_indexer.rb
+++ lib/mas/firm_indexer.rb
@@ -1,30 +1,32 @@
 module FirmIndexer
-  def self.index_firm(firm)
-    if !firm.destroyed? && firm.publishable?
-      store_firm(firm)
-    else
-      delete_firm(firm)
+  class << self
+    def index_firm(firm)
+      if !firm.destroyed? && firm.publishable?
+        store_firm(firm)
+      else
+        delete_firm(firm)
+      end
     end
-  end
 
-  def self.handle_firm_changed(firm)
-    index_firm(firm)
-  end
+    def handle_firm_changed(firm)
+      index_firm(firm)
+    end
 
-  def self.handle_aggregate_changed(aggregate)
-    index_firm(aggregate.firm) if firm_exists?(aggregate.firm)
-  end
+    def handle_aggregate_changed(aggregate)
+      index_firm(aggregate.firm) if firm_exists?(aggregate.firm)
+    end
 
-  def self.store_firm(firm)
-    FirmRepository.new.store(firm)
-  end
+    def store_firm(firm)
+      FirmRepository.new.store(firm)
+    end
 
-  def self.delete_firm(firm)
-    FirmRepository.new.delete(firm.id)
-  end
+    def delete_firm(firm)
+      FirmRepository.new.delete(firm.id)
+    end
 
-  def self.firm_exists?(firm)
-    return false if (firm.nil? || firm.destroyed?)
-    Firm.exists?(firm.id)
+    def firm_exists?(firm)
+      return false if (firm.nil? || firm.destroyed?)
+      Firm.exists?(firm.id)
+    end
   end
 end
