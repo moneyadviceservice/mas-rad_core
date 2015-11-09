ef9f93e7d877f1f0b52ead77fef8a4e76cff6648
Dan Munckton <dan@cultivatehq.com>
2015-11-09 12:14:17 +0000

    Make store_firm and delete_firm private

--- lib/mas/firm_indexer.rb
+++ lib/mas/firm_indexer.rb
@@ -16,17 +16,19 @@ module FirmIndexer
       index_firm(aggregate.firm) if firm_exists?(aggregate.firm)
     end
 
+    def firm_exists?(firm)
+      return false if (firm.nil? || firm.destroyed?)
+      Firm.exists?(firm.id)
+    end
+
+    private
+
     def store_firm(firm)
       FirmRepository.new.store(firm)
     end
 
     def delete_firm(firm)
       FirmRepository.new.delete(firm.id)
-    end
-
-    def firm_exists?(firm)
-      return false if (firm.nil? || firm.destroyed?)
-      Firm.exists?(firm.id)
     end
   end
 end
