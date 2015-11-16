f187c17736afd41c4bb0243d62a472dca41c1873
Dan Munckton <dan@cultivatehq.com>
2015-11-16 14:23:38 +0000

    Remove DeleteFirmJob and IndexFirmJob

--- app/jobs/delete_firm_job.rb
+++
@@ -1,9 +0,0 @@
-class DeleteFirmJob < ActiveJob::Base
-  around_perform do |job, block|
-    Stats.time 'radsignup.delete.firm', &block
-  end
-
-  def perform(id)
-    FirmRepository.new.delete(id)
-  end
-end
