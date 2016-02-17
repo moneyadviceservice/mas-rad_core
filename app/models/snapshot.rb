c751cd3b07e4ffc8a5c2921540f00de89039b2a8
Daniel Grieve <git@cazrin.net>
2016-02-17 14:08:31 +0000

    Refactor some queries to private methods

--- app/models/snapshot.rb
+++ app/models/snapshot.rb
@@ -118,139 +118,115 @@ class Snapshot < ActiveRecord::Base
   end
 
   def query_advisers_who_travel_5_miles
-    Adviser.where(travel_distance: TravelDistance.all['5 miles'])
+    advisers_who_travel('5 miles')
   end
 
   def query_advisers_who_travel_10_miles
-    Adviser.where(travel_distance: TravelDistance.all['10 miles'])
+    advisers_who_travel('10 miles')
   end
 
   def query_advisers_who_travel_25_miles
-    Adviser.where(travel_distance: TravelDistance.all['25 miles'])
+    advisers_who_travel('25 miles')
   end
 
   def query_advisers_who_travel_50_miles
-    Adviser.where(travel_distance: TravelDistance.all['50 miles'])
+    advisers_who_travel('50 miles')
   end
 
   def query_advisers_who_travel_100_miles
-    Adviser.where(travel_distance: TravelDistance.all['100 miles'])
+    advisers_who_travel('100 miles')
   end
 
   def query_advisers_who_travel_150_miles
-    Adviser.where(travel_distance: TravelDistance.all['150 miles'])
+    advisers_who_travel('150 miles')
   end
 
   def query_advisers_who_travel_250_miles
-    Adviser.where(travel_distance: TravelDistance.all['250 miles'])
+    advisers_who_travel('250 miles')
   end
 
   def query_advisers_who_travel_uk_wide
-    Adviser.where(travel_distance: TravelDistance.all['UK wide'])
+    advisers_who_travel('UK wide')
   end
 
   def query_advisers_accredited_in_solla
-    Adviser.includes(:accreditations).where(accreditations: { name: 'SOLLA' })
+    advisers_with_accreditation('SOLLA')
   end
 
   def query_advisers_accredited_in_later_life_academy
-    Adviser.includes(:accreditations).where(accreditations: { name: 'Later Life Academy' })
+    advisers_with_accreditation('Later Life Academy')
   end
 
   def query_advisers_accredited_in_iso22222
-    Adviser.includes(:accreditations).where(accreditations: { name: 'ISO 22222' })
+    advisers_with_accreditation('ISO 22222')
   end
 
   def query_advisers_accredited_in_bs8577
-    Adviser.includes(:accreditations).where(accreditations: { name: 'British Standard in Financial Planning BS8577' })
+    advisers_with_accreditation('British Standard in Financial Planning BS8577')
   end
 
   def query_advisers_with_qualification_in_level_4
-    Adviser.includes(:qualifications).where(qualifications: { name: 'Level 4 (DipPFS, DipFA® or equivalent)' })
+    advisers_with_qualification('Level 4 (DipPFS, DipFA® or equivalent)')
   end
 
   def query_advisers_with_qualification_in_level_6
-    Adviser.includes(:qualifications).where(qualifications: { name: 'Level 6 (APFS, Adv DipFA®)' })
+    advisers_with_qualification('Level 6 (APFS, Adv DipFA®)')
   end
 
   def query_advisers_with_qualification_in_chartered_financial_planner
-    Adviser.includes(:qualifications).where(qualifications: { name: 'Chartered Financial Planner' })
+    advisers_with_qualification('Chartered Financial Planner')
   end
 
   def query_advisers_with_qualification_in_certified_financial_planner
-    Adviser.includes(:qualifications).where(qualifications: { name: 'Certified Financial Planner' })
+    advisers_with_qualification('Certified Financial Planner')
   end
 
   def query_advisers_with_qualification_in_pension_transfer
-    Adviser.includes(:qualifications).where(qualifications: {
-      name: 'Pension transfer qualifications - holder of G60, AF3, AwPETR®, or equivalent'
-    })
+    advisers_with_qualification('Pension transfer qualifications - holder of G60, AF3, AwPETR®, or equivalent')
   end
 
   def query_advisers_with_qualification_in_equity_release
-    Adviser.includes(:qualifications).where(qualifications: {
-      name: 'Equity release qualifications i.e. holder of Certificate in Equity Release or equivalent'
-    })
+    advisers_with_qualification('Equity release qualifications i.e. holder of Certificate in Equity Release or equivalent')
   end
 
   def query_advisers_with_qualification_in_long_term_care_planning
-    Adviser.includes(:qualifications).where(qualifications: {
-      name: 'Long term care planning qualifications i.e. holder of CF8, CeLTCI®. or equivalent'
-    })
+    advisers_with_qualification('Long term care planning qualifications i.e. holder of CF8, CeLTCI®. or equivalent')
   end
 
   def query_advisers_with_qualification_in_tep
-    Adviser.includes(:qualifications).where(qualifications: {
-      name: 'Holder of Trust and Estate Practitioner qualification (TEP) i.e. full member of STEP'
-    })
+    advisers_with_qualification('Holder of Trust and Estate Practitioner qualification (TEP) i.e. full member of STEP')
   end
 
   def query_advisers_with_qualification_in_fcii
-    Adviser.includes(:qualifications).where(qualifications: {
-      name: 'Fellow of the Chartered Insurance Institute (FCII)'
-    })
+    advisers_with_qualification('Fellow of the Chartered Insurance Institute (FCII)')
   end
 
   def query_advisers_part_of_personal_finance_society
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'Personal Finance Society / Chartered Insurance Institute'
-    })
+    advisers_part_of('Personal Finance Society / Chartered Insurance Institute')
   end
 
   def query_advisers_part_of_institute_financial_planning
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'Institute of Financial Planning'
-    })
+    advisers_part_of('Institute of Financial Planning')
   end
 
   def query_advisers_part_of_institute_financial_services
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'Institute of Financial Services'
-    })
+    advisers_part_of('Institute of Financial Services')
   end
 
   def query_advisers_part_of_ci_bankers_scotland
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'The Chartered Institute of Bankers in Scotland'
-    })
+    advisers_part_of('The Chartered Institute of Bankers in Scotland')
   end
 
   def query_advisers_part_of_ci_securities_and_investments
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'The Chartered Institute for Securities and Investments'
-    })
+    advisers_part_of('The Chartered Institute for Securities and Investments')
   end
 
   def query_advisers_part_of_cfa_institute
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'CFA Institute'
-    })
+    advisers_part_of('CFA Institute')
   end
 
   def query_advisers_part_of_chartered_accountants
-    Adviser.includes(:professional_bodies).where(professional_bodies: {
-      name: 'Institute of Chartered Accountants for England and Wales'
-    })
+    advisers_part_of('Institute of Chartered Accountants for England and Wales')
   end
 
   def run_queries_and_save
@@ -264,6 +240,22 @@ class Snapshot < ActiveRecord::Base
     postcodes = advisers.map { |adviser| adviser.postcode }
     country_postcodes = country_postcodes(postcodes, country)
     advisers.select { |adviser| country_postcodes.include?(adviser.postcode) }
+  end
+
+  def advisers_part_of(professional_body)
+    Adviser.includes(:professional_bodies).where(professional_bodies: { name: professional_body })
+  end
+
+  def advisers_who_travel(distance)
+    Adviser.where(travel_distance: TravelDistance.all[distance])
+  end
+
+  def advisers_with_accreditation(accreditation)
+    Adviser.includes(:accreditations).where(accreditations: { name: accreditation })
+  end
+
+  def advisers_with_qualification(qualification)
+    Adviser.includes(:qualifications).where(qualifications: { name: qualification })
   end
 
   def country_postcodes(postcodes, country)
