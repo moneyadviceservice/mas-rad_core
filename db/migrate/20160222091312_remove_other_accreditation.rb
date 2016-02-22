98943b0207efe4c89eece6191e1ff21edaa775a7
Daniel Grieve <git@cazrin.net>
2016-02-22 10:17:59 +0000

    Remove irreversible migration and add comment

--- db/migrate/20160222091312_remove_other_accreditation.rb
+++ db/migrate/20160222091312_remove_other_accreditation.rb
@@ -7,6 +7,13 @@ class RemoveOtherAccreditation < ActiveRecord::Migration
   end
 
   def down
-    fail ActiveRecord::IrreversibleMigration
+    # Unfortunately we can't just insert the 'Other' record, as there are parts
+    # of `rad` and `rad_consumer` which rely on the 'Other' record having the
+    # ID of '5'. This is due to translations and configuring whether or not to
+    # show the record.
+    #
+    # You're allowed to roll back this migration, but be aware that if you're
+    # using an older version of `rad` or `rad_consumer` they may throw errors
+    # if this record doesn't exist.
   end
 end
