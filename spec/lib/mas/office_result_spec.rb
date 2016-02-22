dcfe33ea583502d2a04ceff2e036dd09af8e3c14
Dan Munckton <dan@cultivatehq.com>
2016-02-22 11:59:51 +0000

    Remove OfficeResult#telephone_number

--- spec/lib/mas/office_result_spec.rb
+++ spec/lib/mas/office_result_spec.rb
@@ -8,7 +8,7 @@ RSpec.describe OfficeResult do
       'address_county'   => 'Cumbria',
       'address_postcode' => 'LA8 9BE',
       'email_address'    => 'postie@example.com',
-      'telephone_number' => '02085952346',
+      'telephone_number' => '020 8595 2346',
       'disabled_access'  => true,
       'location' => { 'lat' => 51.5180697, 'lon' => -0.1085203 }
     }
